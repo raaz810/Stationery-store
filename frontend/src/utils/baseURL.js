2081f7bc1a30f05b8555596b9044b0cfac056816
raaz810 <raaz810218@gmail.com>
2025-04-21 10:25:36 +0530

    url changed

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,14 +1,7 @@
 const getBaseUrl = () => {
-    //return "https://skribb-backend.vercel.app"
-    return "http://localhost:8000"
+    return "https://stationery-store.onrender.com"
+    //return "http://localhost:8000"
 }
 
 export default getBaseUrl;
 
-// const getBaseUrl = () => {
-//     return process.env.NODE_ENV === "development"
-//         ? "http://localhost:8000"
-//         : "https://skribb-backend.vercel.app";
-// };
-
-// export default getBaseUrl;
